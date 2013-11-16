9c611d37b2c7a055eb7c25604ea7a14477eef46f
gaxit <gaxit@interia.eu>
2013-11-16 13:38:33 +0100

    Dodanie kodowania utf-8 Zmiana dodawanych danych w pliku import.sql

--- src/main/java/test/TestBean.java
+++ src/main/java/test/TestBean.java
@@ -7,19 +7,21 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
 import pl.rea.hibernate.HibernateUtil;
+import pl.rea.model.Role;
 import pl.rea.model.Status;
 
 @ManagedBean(name="testBean")
 public class TestBean {
 	public void test(){
-		System.out.println("Trollllll");
 		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 		Session session = sessionFactory.openSession();
-//		session.close();
 		Transaction tx = session.beginTransaction();
-		Status status = new Status();
-		status.setStatus("trol");
-//		session.save(status);
+		
+		Role status = new Role();
+		status.setId(3);
+		status.setRole("trołŁ");
+		session.save(status);
+		
 		tx.commit();
 		session.close();
 	}
