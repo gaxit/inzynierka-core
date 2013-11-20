4d733d2bdeffdd4fe5780161011834965aca818e
mieszkodlugosz <mieszkod@gmail.com>
2013-11-20 14:47:31 +0100

    Mam wrażenie że commity nie zawsze działają. Testing...

--- src/main/java/test/TestBean.java
+++ src/main/java/test/TestBean.java
@@ -161,6 +161,7 @@ public class TestBean {
 		testgetUserRoles(session);
 		createTestRole(session);
 		testgetUserRoles(session);
+		System.out.println("successful test is successful");
 		
 		System.out.println("trololo");
 //		session.save(status);
