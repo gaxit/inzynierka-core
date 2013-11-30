622ad2d7dd4b2025ddacd1c3e0815d215f193498
gaxit <gaxit@interia.eu>
2013-11-30 19:15:45 +0100

    Dodano szkielet metod dla webservicow

---
+++ src/main/java/pl/rea/webservices/OfferService.java
@@ -0,0 +1,60 @@
+package pl.rea.webservices;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.jws.WebService;
+
+import pl.rea.canonical.OfferCanonical;
+
+@WebService(serviceName = "userService")
+@Stateless
+public class OfferService {
+
+	public void addOfferToUserFavourites(String login, String sessionId,
+			Long offerId, String userLoginToAddOffer) {
+		// sprawdzanie, czy dany uzytkownik jest zalogowany
+		// sprawdzenie, czy zalogowany jest admin lub login i userLoginToAddOffer jest taki sam
+		// pobranie uzytkownika userLoginToAddOffer
+		// pobranie oferty o podanym id
+		// dodanie do jego listy ulubionych pobranej oferty
+		// update uzytkownika
+	}
+
+	public List<OfferCanonical> getUserFavouritesOffers(String login,
+			String sessionId, String userFavouritesLogin) {
+		// sprawdzenie, czy uzytkownik jest zalogowany
+		// sprawdzenie, czy zalogowany jest admin albo login i userFavouritesLogin sa takie same
+		// pobranie uzytkownika o podanym loginie
+		// pobranie jego listy ulubionych
+		// konwersja tej listy do listy OfferCanonical - metoda z pakietu
+		// transform
+		// zwrocenie tej listy
+
+		return null;
+	}
+
+	public void deleteOfferFromUserFavourites(String login, String sessionId,
+			Long offerId, String userFavouritesLogin) {
+		// sprawdzenie, czy uzytkownik jest zalogowany
+		// sprawdzenie, czy zalogowany jest admin badz login i userFavouritesLogin sa takie same
+		// pobranie uzytkownika o podanym loginie
+		// pobranie jego listy ulubionych
+		// usuniecie z jego listy ulubionych oferty o podanym id
+	}
+
+	public List<OfferCanonical> findOffers(int minPrice, int maxPrice,
+			int minArea, int maxArea, String estateType, String town,
+			boolean isGarage, int floor) {
+		// wyszukanie ofert o podanych kryteriach
+
+		return null;
+	}
+	
+	public List<OfferCanonical> getAllOffers(){
+		// pobranie wszystkich ofert dostepnych w bazie
+		
+		return null;
+	}
+
+}
---
+++ src/main/java/pl/rea/webservices/UserService.java
@@ -0,0 +1,76 @@
+package pl.rea.webservices;
+
+import javax.ejb.Stateless;
+import javax.jws.WebService;
+
+import pl.rea.canonical.UserCanonical;
+
+@WebService(serviceName = "userService")
+@Stateless
+public class UserService {
+
+	public boolean isAnybodyLogged(String login, String sessionId) {
+		// sprawdzanie, czy ktokolwiek jest zalogowany
+		// wywolanie metody LoggedUserUtils.isLogged
+		return false;
+	}
+
+	public boolean isUserLogged(String login, String sessionId) {
+		// sprawdzanie, czy uzytkownik jest zalogowany
+		// wywolanie metody LoggedUserUtils.isUserLogged
+		return false;
+	}
+
+	public boolean isAdminLogged(String login, String sessionId) {
+		// sprawdzanie, czy admin jest zalogowany
+		// wywolanie metody LoggedUserUtils.isAdminLogged
+		return false;
+	}
+
+	public String logIn(String login) {
+		// zalogowanie uzytkownika
+		// trzeba pobrac uzytkownika z bazy o podanym loginie
+		// obliczyc dla takiego loginu sessionId
+		// uzupelnic pole sessionId pobranego uzytkownika
+		// wywolac metode userDao.updateUser(pobranyUser)
+		// zwrocic sessionId - return sessionId
+
+		return null;
+	}
+
+	public void logOut(String login, String sessionId) {
+		// wylogowanie uzytkownika
+		// najpierw sprawdzic, czy taki uzytkownik jest zalogowany -
+		// LoggedUserUtils.isLogged
+		// trzeba pobrac uzytkownika z bazy o podanym loginie
+		// ustawic pole sessionId pobranego uzytkownika na null
+		// wywolac metode userDao.updateUser(pobranyUser)
+	}
+
+	public void createUser(UserCanonical user) {
+		// dodanie nowego uzytkownika - przez rejestracje juz admina
+		// konwersja UserCanonical na User - bazodanowy - metoda z pakietu
+		// transform
+		// zapisanie uzytkownika do bazy
+	}
+
+	public void editUser(String login, String sessionId, UserCanonical user) {
+		// edytowanie uzytkownika
+		// sprawdzanie, czy uzytkownik o loginie login jest zalogowany
+		// sprawdzenie, czy zalogowany jest admin albo login i user_login sa takie same
+		// dlatego tak, zeby wykorzystac jedna metode dla edytowania przez admina i przez uzytkownika
+		// gdy uzytkownik bedzie edytowal swoje dane, to login i login_z_user beda takie same
+		// na poczatku sprawdzanie, czy ten uzytkownik jest zalogowany
+		// konwersja UserCanonical na User - bazodanowy - metoda z pakietu
+		// transform
+		// update uzytkownika w bazie
+	}
+
+	public void deleteUser(String login, String sessionId,
+			String userLoginToDelete) {
+		// sprawdzenie, czy admin jest zalogowany - moznaby tylko jemu dac prawo
+		// do usuwania, co Ty na to?
+		// usuniecie uzytkownika o podanym loginie
+	}
+
+}
