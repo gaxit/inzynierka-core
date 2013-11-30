4ff4daa3e379f6c224a35f0928f38b6083e07834
gaxit <gaxit@interia.eu>
2013-11-30 16:42:34 +0100

    Poprawiono i dodano nowe poczatkowe importy

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,23 +1,29 @@
 
-INSERT INTO estatetype (id, estatetype) VALUES (1, 'MIESZKANIE');
-INSERT INTO estatetype (id, estatetype) VALUES (2, 'DOM');
-INSERT INTO estatetype (id, estatetype) VALUES (3, 'DZIAŁKA');
-INSERT INTO role (id, role) VALUES (1, 'ADMIN');
-INSERT INTO role (id, role) VALUES (2, 'USER');
-INSERT INTO role (id, role) VALUES (42, 'USER');
-INSERT INTO transactiontype (id, transactiontype) VALUES (1, 'WYNAJEM');
-INSERT INTO transactiontype (id, transactiontype) VALUES (2, 'SPRZEDAŻ');
-INSERT INTO address (id, apartmentno, houseno, postalcode, street, town) VALUES (3,5,5,'12345','KRAKOWSKA','KRAKOW');
-INSERT INTO userek (user_id,email,login,name,password,phonenumber,address_id,role_id) VALUES (42,'email1','test1','Testowy1','test','2345',3,42);
-INSERT INTO userek (user_id,email,login,name,password,phonenumber,address_id,role_id) VALUES (43,'email2','test2','Testowy2','test','2345',3,2);
---INSERT INTO offer_offer (id, favorites_id) VALUES (42,42);
---INSERT INTO offer_offer (id, favorites_id) VALUES (43,43);
+INSERT INTO estatetype (id, estatetype) VALUES (10001, 'Mieszkanie');
+INSERT INTO estatetype (id, estatetype) VALUES (10002, 'Dom');
+INSERT INTO estatetype (id, estatetype) VALUES (10003, 'Dzialka');
 
---INSERT INTO estatetype (id, estatetype) VALUES (1, 'Mieszkanie');
---INSERT INTO estatetype (id, estatetype) VALUES (2, 'Dom');
---INSERT INTO estatetype (id, estatetype) VALUES (3, 'Dzialka');
---INSERT INTO role (id, role) VALUES (3, 'Administrator');
---INSERT INTO role (id, role) VALUES (2, 'Uzytkownik');
---INSERT INTO transactiontype (id, transactiontype) VALUES (1, 'Wynajem');
---INSERT INTO transactiontype (id, transactiontype) VALUES (2, 'Sprzedaz');
---INSERT INTO transactiontype (id, transactiontype) VALUES (2, 'Sprzedaz');
+INSERT INTO role (id, role) VALUES (10001, 'Administrator');
+INSERT INTO role (id, role) VALUES (10002, 'Uzytkownik');
+
+INSERT INTO transactiontype (id, transactiontype) VALUES (10001, 'Wynajem');
+INSERT INTO transactiontype (id, transactiontype) VALUES (10002, 'Sprzedaz');
+
+INSERT INTO address (id, apartmentno, houseno, postalcode, street, town) VALUES (10001, 5, 5,'12-345', 'Florianska', 'Krakow');
+
+INSERT INTO userek (user_id, email, login, name, password, phonenumber, address_id, role_id) VALUES (10001, 'mail1@mail.com', 'login1', 'imie1', 'haslo1', 'nrTelefonu1', 10001, 10001);
+INSERT INTO userek (user_id, email, login, name, password, phonenumber, address_id, role_id) VALUES (10002, 'mail2@mail.com', 'login2', 'imie2', 'haslo2', 'nrTelefonu2', 10001, 10001);
+INSERT INTO userek (user_id, email, login, name, password, phonenumber, address_id, role_id, sessionid) VALUES (10003, 'mail2@mail.com', 'login3', 'imie2', 'haslo2', 'nrTelefonu2', 10001, 10001, 'sessionid1');
+
+INSERT INTO offer (offer_id, area, creationdate, description, finishdate, floor, garage, price, address_id, estatetype_id, transactiontype_id) VALUES (10001, 100, '1971-07-13', 'Opis1', '1971-07-13', '3', true, 200000, 10001, 10001, 10001);
+INSERT INTO offer (offer_id, area, creationdate, description, finishdate, floor, garage, price, address_id, estatetype_id, transactiontype_id) VALUES (10002, 150, '1972-07-13', 'Opis2', '1972-07-13', '2', false, 400000, 10001, 10002, 10001);
+INSERT INTO offer (offer_id, area, creationdate, description, finishdate, floor, garage, price, address_id, estatetype_id, transactiontype_id) VALUES (10003, 100, '1973-07-13', 'Opis3', '1973-07-13', '5', true, 500000, 10001, 10001, 10001);
+INSERT INTO offer (offer_id, area, creationdate, description, finishdate, floor, garage, price, address_id, estatetype_id, transactiontype_id) VALUES (10004, 100, '1974-07-13', 'Opis4', '1974-07-13', '3', true, 200000, 10001, 10001, 10001);
+
+INSERT INTO owner_offer (user_id, offer_id) VALUES (10001, 10001);
+INSERT INTO owner_offer (user_id, offer_id) VALUES (10001, 10002);
+INSERT INTO owner_offer (user_id, offer_id) VALUES (10002, 10003);
+INSERT INTO owner_offer (user_id, offer_id) VALUES (10002, 10004);
+
+INSERT INTO favouries (user_id, offer_id) VALUES (10003, 10001);
+INSERT INTO favouries (user_id, offer_id) VALUES (10003, 10003);
