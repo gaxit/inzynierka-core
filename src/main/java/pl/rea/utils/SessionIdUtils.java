01c4baf424b257c8fcfeb9279e5bffa8b414e8e4
mieszkodlugosz <mieszkod@gmail.com>
2013-11-30 17:23:41 +0100

    Dodana jedna metoda...

--- src/main/java/pl/rea/utils/SessionIdUtils.java
+++ src/main/java/pl/rea/utils/SessionIdUtils.java
@@ -2,4 +2,16 @@ package pl.rea.utils;
 
 public class SessionIdUtils {
 
+	public static String getSessionId(String login)
+	{
+		String sessionId=null;
+		long time=System.currentTimeMillis();
+		
+		long loginHash = (long)login.hashCode();
+		time=(long) (time*(loginHash*0.37));
+		time=time-loginHash*(17-loginHash%10);
+		sessionId=String.valueOf(time);
+		return sessionId;
+	}
+	
 }
