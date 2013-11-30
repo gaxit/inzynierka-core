17396a609c9b7374b65dee0d9bed2ac19a19941d
gaxit <gaxit@interia.eu>
2013-11-30 17:15:45 +0100

    User logged fix

--- src/main/java/pl/rea/utils/LoggedUserUtils.java
+++ src/main/java/pl/rea/utils/LoggedUserUtils.java
@@ -12,23 +12,18 @@ public class LoggedUserUtils {
 	
 	public boolean isUserLogged(String login, String sessionId){
 		if (login==null || sessionId==null){
-			System.out.println("Dane z argumentów są nullem");
 			return false;
 		}
 		User userDB = userDao.getUserByLogin(login);
 		if (userDB==null){
-			System.out.println("Nie znalezionon uzytkownika");
 			return false;
 		}
 		if (userDB.getSessionId()==null){
-			System.out.println("Uzytkownik nie ma session id");
 			return false;
 		}
 		if (sessionId.equals(userDB.getSessionId())){
-			System.out.println("POPRAWNIE uzytkownik jest zalogowany");
 			return true;
 		}
-		System.out.println("Session id różne");
 		return false;
 	}
 
